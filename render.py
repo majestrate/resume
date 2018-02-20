06223ade2d8f339c115cba27ee07fd6893997e8c
Jeff Becker <jeff@i2p.rocks>
2018-02-20 10:26:00 -0500

    add footer, refactor other templates

--- render.py
+++ render.py
@@ -1,24 +1,12 @@
 import jinja2
 import markdown
 
-def renderFile(t, fname, w):
-    """
-    render a markdown file
-    :param t: a jinja2 template
-    :param fname: the filename of the markdown file
-    :param w: a file object to write output to
-    """
-    with open(fname) as f:
-        html = markdown.markdown(f.read(), output_format='html5')
-        fullhtml = t.render(body=html)
-        w.write(fullhtml)
-
 def main():
     env = jinja2.Environment(loader=jinja2.FileSystemLoader("templates"))
     t = env.get_template('resume.jinja2')
     with open('cv.html', 'w') as w:
         parts = dict()
-        for f in ('header', 'body', 'about'):
+        for f in ('header', 'body', 'about', 'footer'):
             with open('{}.md'.format(f)) as r:
                 parts[f] = markdown.markdown(r.read(), output_format='html5')
         w.write(t.render(**parts))
